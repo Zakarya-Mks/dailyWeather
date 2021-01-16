bace8e27ea989be004cd495817172d9f44ecdd79
Zakarya-Mks <zakaaryaaa@gmail.com>
2021-01-16 11:03:01 +0100

    implemented the ui

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,2 @@
 import 'bootstrap';
 import '../scss/style.scss';
-
-const button = document.createElement('button');
-
-button.classList.add('btn', 'btn-lg', 'btn-outline-danger'),
-  (button.textContent = 'click Me');
-
-document.querySelector('#root').appendChild(button);
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
+    host: '0.0.0.0',
     port: 9000,
   },
   module: {
@@ -39,11 +41,46 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024,
+          name: 'img/[hash].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024,
+          // Remove quotes around the encoded URL –
+          // they’re rarely useful
+          noquotes: true,
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre',
+        options: {
+          bypassOnDebug: true, // webpack@1.x
+          disable: true, // webpack@2.x and newer
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'DailyWeather',
       template: 'src/template.html',
     }),
   ],
