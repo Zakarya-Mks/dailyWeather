34724aaf68b6de3633ae2553f91c1400c5f7828f
Zakarya-Mks <zakaaryaaa@gmail.com>
2021-01-18 19:04:20 +0100

    fix: add temp unit to the daily weatherForecast

--- src/js/view.js
+++ src/js/view.js
@@ -36,22 +36,32 @@ export class View {
 
     // api daily arr contain 8 day,
     // only need 7 so setting the max index to 7
+    const tempUnit = localStorage.getItem('temperatureUnit')
+      ? localStorage.getItem('temperatureUnit')
+      : 'C';
     for (let index = 1; index < 7; index++) {
       const day = weatherDate.daily[index];
       const date = helperFunctions.timeConverter(day.dt);
+
       domElemCollection[`day_${index}_date`].textContent = date.day;
+
       domElemCollection[`day_${index}_icon`].src = require('../img/icons/' +
         day.weather[0].icon.substring(0, 2) +
         '.svg');
-      const convertedMaxTemp = helperFunctions.convertTemp(day.temp.max);
-      domElemCollection[
-        `day_${index}_maxTemp`
-      ].textContent = helperFunctions.truncateTemp(convertedMaxTemp);
-      const convertedMinTemp = helperFunctions.convertTemp(day.temp.min);
-      domElemCollection[
-        `day_${index}_minTemp`
-      ].textContent = helperFunctions.truncateTemp(convertedMinTemp);
+
+      const truncatedMaxTemp = Math.trunc(
+        helperFunctions.convertTemp(day.temp.max)
+      );
+      domElemCollection[`day_${index}_maxTemp`].textContent =
+        truncatedMaxTemp + '°' + tempUnit;
+
+      const truncatedMinTemp = Math.trunc(
+        helperFunctions.convertTemp(day.temp.min)
+      );
+      domElemCollection[`day_${index}_minTemp`].textContent =
+        truncatedMinTemp + '°' + tempUnit;
     }
+
     if (domElemCollection.mainWeatherSection.classList.contains('d-none')) {
       domElemCollection.loadingPageAnimation.classList.add('d-none');
 
